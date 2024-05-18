de3d6f7c2a9f9fdf2c16729cce003fab3fa26e77
piyush0431 <rana.workslance@gmail.com>
2024-05-19 00:46:00 +0530

    Update Students

--- app/students/[student_profile]/student.js
+++ app/students/[student_profile]/student.js
@@ -3,24 +3,24 @@ import React from 'react';
 export default function Page({ params }) {
     return (
         <>
-            <div>Student: {params.firstname}</div>
-            <div>Student: {params.lastname}</div>
-            <div>Student: {params.address}</div>
-            <div>Student: {params.joined_institute}</div>
-            <div>Student: {params.age}</div>
-            <div>Student: {params['student-email']}</div>
-            <div>Student: {params['phone-student']}</div>
-            <div>Student: {params['phone-student-ot-opt']}</div>
-            <div>Student: {params.gender}</div>
-            <div>Student: {params.nationality}</div>
-            <div>Student: {params.department}</div>
-            <div>Student: {params['year-complition']}</div>
-            <div>Student: {params['parent-name']}</div>
-            <div>Student: {params['medical-information']}</div>
-            <div>Student: {params['student-dashboard']}</div>
-            <div>Student: {params['max-cgpa-earned']}</div>
-            <div>Student: {params.is_enrolled}</div>
-            <div>Student: {params['about_student']}</div>
+            <div>Student First Name: {params.firstname}</div>
+            <div>Student Last Name: {params.lastname}</div>
+            <div>Student Address: {params.address}</div>
+            <div>Student Joined Institute: {params.joined_institute}</div>
+            <div>Student Age: {params.age}</div>
+            <div>Student Email: {params['student-email']}</div>
+            <div>Student Phone: {params['phone-student']}</div>
+            <div>Student Phone (alt): {params['phone-student-ot-opt']}</div>
+            <div>Student Gender: {params.gender}</div>
+            <div>Student Nationality: {params.nationality}</div>
+            <div>Student Department: {params.department}</div>
+            <div>Student Complition Year: {params['year-complition']}</div>
+            <div>Student Parent: {params['parent-name']}</div>
+            <div>Student Medical Info.: {params['medical-information']}</div>
+            <div>Student Dashboard Link: {params['student-dashboard']}</div>
+            <div>Student Earned CGPA max: {params['max-cgpa-earned']}</div>
+            <div>Student Is Enroled: {params.is_enrolled}</div>
+            <div>About Student: {params['about_student']}</div>
         </>
     );
 }
