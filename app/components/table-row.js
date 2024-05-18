212c0e9a79d8136a4b805268c8d84721e1dcc985
piyush0431 <rana.workslance@gmail.com>
2024-05-19 01:02:43 +0530

    Update The Tablwe-rows and Student js

--- app/components/table-row.js
+++ app/components/table-row.js
@@ -1,14 +1,47 @@
-import React from 'react'
+"use client"
+
+import useState from 'react'
+import { Client, Databases, Query } from 'appwrite';
 
 function TableRow(props) {
+    
+
+    const [student, setStudent] = useState();
+
+    const client = new Client()
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject('66487fe600145ff0181e');
+
+    const databases = new Databases(client);
+
+    useEffect(() => {
+        const fetchStudents = async () => {
+            try {
+                const response = await databases.listDocuments(
+                    'db.appwrt.pixelthreader749404',
+                    'db.pixelthreader.studentsdbms4141',
+                    [
+                        Query.equal('$id', props?.studentId)
+                    ]
+                );
+                setStudent(response.documents[0]);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchStudents();
+    }, []);
+
+
     return (
-        <tr role='button' className='table-row' target={props.studentId}>
-            <th scope="row">{props.sno}</th>
-            <td>{props.firstname}</td>
-            <td>{props.lastname}</td>
+        <tr role='button' className='table-row' target={props?.studentId}>
+            <th scope="row">{props?.sno}</th>
+            <td>{props?.firstname}</td>
+            <td>{props?.lastname}</td>
             <td className='d-flex justify-content-start align-items-center gap-2'>
-                <button target={props.studentId} className="btn btn-sm btn-info">Edit</button>
-                <button target={props.studentId} className="btn btn-sm btn-danger">Delete</button>
+                <button target={props?.studentId} className="btn btn-sm btn-info">Edit</button>
+                <button target={props?.studentId} className="btn btn-sm btn-danger">Delete</button>
             </td>
         </tr>
     )
--- app/students/[student_profile]/student.js
+++ app/students/[student_profile]/student.js
@@ -3,25 +3,39 @@ import React from 'react';
 export default function Page({ params }) {
     return (
         <>
-            <div>Student ID: {params.$id}</div>
-            <div>Student First Name: {params.firstname}</div>
-            <div>Student Last Name: {params.lastname}</div>
-            <div>Student Address: {params.address}</div>
-            <div>Student Joined Institute: {params.joined_institute}</div>
-            <div>Student Age: {params.age}</div>
-            <div>Student Email: {params['student-email']}</div>
-            <div>Student Phone: {params['phone-student']}</div>
-            <div>Student Phone (alt): {params['phone-student-ot-opt']}</div>
-            <div>Student Gender: {params.gender}</div>
-            <div>Student Nationality: {params.nationality}</div>
-            <div>Student Department: {params.department}</div>
-            <div>Student Complition Year: {params['year-complition']}</div>
-            <div>Student Parent: {params['parent-name']}</div>
-            <div>Student Medical Info.: {params['medical-information']}</div>
-            <div>Student Dashboard Link: {params['student-dashboard']}</div>
-            <div>Student Earned CGPA max: {params['max-cgpa-earned']}</div>
-            <div>Student Is Enroled: {params.is_enrolled}</div>
-            <div>About Student: {params['about_student']}</div>
+            <div className="container py-5">
+                <h1 className="display-5 fw-bold text-center my-3">Student Information</h1>
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">Student ID: {params.$id}</h5>
+                                <p className="card-text">
+                                    Student First Name: {params.firstname}<br />
+                                    Student Last Name: {params.lastname}<br />
+                                    Student Address: {params.address}<br />
+                                    Student Joined Institute: {params.joined_institute}<br />
+                                    Student Age: {params.age}<br />
+                                    Student Email: {params['student-email']}<br />
+                                    Student Phone: {params['phone-student']}<br />
+                                    Student Phone (alt): {params['phone-student-ot-opt']}<br />
+                                    Student Gender: {params.gender}<br />
+                                    Student Nationality: {params.nationality}<br />
+                                    Student Department: {params.department}<br />
+                                    Student Complition Year: {params['year-complition']}<br />
+                                    Student Parent: {params['parent-name']}<br />
+                                    Student Medical Info.: {params['medical-information']}<br />
+                                    Student Dashboard Link: {params['student-dashboard']}<br />
+                                    Student Earned CGPA max: {params['max-cgpa-earned']}<br />
+                                    Student Is Enroled: {params.is_enrolled ? 'Yes' : 'No'}<br />
+                                    About Student: {params['about_student']}<br />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
+
